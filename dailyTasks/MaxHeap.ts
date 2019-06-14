951c380a7a469a8a8290dcbe010aaf13f4642a2c
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-06-14 11:38:37 -0500

    minHeap daily task

--- dailyTasks/MaxHeap.ts
+++ dailyTasks/MaxHeap.ts
@@ -53,3 +53,77 @@ class MaxHeap {
 }
 
 module.exports = MaxHeap;
+
+
+
+
+
+
+
+
+
+
+class MinHeap{
+  arr: number[]
+  constructor(){
+    this.arr = [null];
+  }
+
+  push(val: number){
+    this.arr.push(val);
+    this.swim(this.arr.length - 1);
+  }
+
+  delMin(){
+    const result = this.arr[1];
+    const end = this.arr[this.arr.length - 1];
+
+    this.arr[1] = end;
+    this.arr.pop();
+    this.sink(1);
+
+    return result;
+  }
+
+  swim(idx: number){
+    const candidate = this.arr[idx];
+    while (idx > 1){
+      const parentIdx = Math.floor(idx / 2);
+      const parent = this.arr[parentIdx];
+
+      if (parent < candidate) break;
+
+      this.arr[parentIdx] = candidate;
+      this.arr[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  sink(idx: number){
+    const candidate = this.arr[idx];
+
+    while (true){
+      const child1 = this.arr[idx * 2]; 
+      const child2 = this.arr[idx * 2 + 1];
+      
+      let swapIdx;
+      if (child1 < candidate){
+        swapIdx = idx * 2;
+      }
+      if (child2 < child1){
+        swapIdx++;
+      }
+
+      if (!swapIdx) break;
+
+      this.arr[idx] = this.arr[swapIdx];
+      this.arr[swapIdx] = candidate;
+      idx = swapIdx;
+    }
+
+  }
+
+
+}
+
+
