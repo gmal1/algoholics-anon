4f291f58f22246ff68d78bde20d49ac38f1732e0
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-06-15 13:39:11 -0500

    lngestSubstr comments

---
+++ algorithms/sorts/quick.ts
@@ -0,0 +1,9 @@
+function quickSort(arr: number[]): number[] {
+  return arr;
+}
+
+function swap(arr: number[], i: number, j: number): void {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+function partition(arr: number[], i: number, j: number): void {}
--- interview-questions/longestSubStrNoRepeat.ts
+++ interview-questions/longestSubStrNoRepeat.ts
@@ -4,28 +4,34 @@
  * return the size of the longest substring that doesnt have repeat chars
  */
 
-/**
+/*
  * important ideas:
  * ask interviewer if we can assume only lower case letters, only ASCII, etc.
  * if bounded to only 26 lowercase, we can use a 26 entry map and argue that it is constant space (independent of string size)
  *
- * brute force would be quadratic
+ * brute force would be quadratic time, also constant space
  *
  * sliding window problem category
- * -expand the window when you can, keep track of "most recent" (rightmost) instance of each character in window
- * -when condition is broken (repeat character) contract window as far right as is needed
+ * -linear time, right side of window never stops moving until the end, never backtracks
+ * -expand the window when you can, keep track of "most recent" (rightmost) instance of each character seen so far
+ * -when condition is broken (repeat character) contract window as far right as needed
  */
 
 function longest(str: string): number {
-  let max = 0;
   const map = new Map();
+  let max = 0;
   let left = 0;
   for (let right = 0; right < str.length; right++) {
     const cand = str[right];
     const previousIdx = map.get(cand);
+
+    // if previousIdx is defined and is within the window (window = right - left)
+    // contract the window  past the previous idx of this character
     left = previousIdx >= left ? previousIdx + 1 : left;
-    max = Math.max(max, right - left + 1);
+
+    // set the new rightmost idx of this character and update max if window is bigger
     map.set(cand, right);
+    max = Math.max(max, right - left + 1);
   }
   return max;
 }
