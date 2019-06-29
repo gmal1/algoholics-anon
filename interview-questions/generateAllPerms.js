fcb99e1f47382452a9ca12b6532487f14549c448
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-06-29 15:19:02 -0400

    perms

---
+++ interview-questions/generateAllPerms.js
@@ -0,0 +1,24 @@
+/**
+given an array of strings, generate all unique permutations of each string
+ */
+
+function perms(...sArr) {
+  const output = new Set();
+
+  function helper(str, acc) {
+    if (str.length === 0) return output.add(acc);
+    for (let i = 0; i < str.length; i++) {
+      const char = str[i];
+      acc += char;
+      helper(str.slice(0, i) + str.slice(i + 1), acc);
+      acc = acc.slice(0, acc.length - 1);
+    }
+  }
+
+  for (let s of sArr) {
+    helper(s, '');
+  }
+  return [...output];
+}
+
+perms('sac', 'acs', 'cas', 'sas', 'hi');
