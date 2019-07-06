ec987c54d5742d426ab19ad2a3c1d6e568267d3c
Nel Malikova <gunelmalikova@gmail.com>
2019-07-06 11:28:38 -0400

    merge, update readme

--- algorithms/sorts/wiggleSort.js
+++ algorithms/sorts/wiggleSort.js
@@ -9,10 +9,10 @@ if input numbers = [3, 5, 2, 1, 6, 4]
 one possible Wiggle Sorted answer is [3, 5, 1, 6, 2, 4]. 
  */
 
-function wiggleSort(nums){
-	for (let i = 1; i < nums.length; i++){
-		if ((i%2 === 1) === (nums[i-1]>nums[i])){
-			[nums[i-1],nums[i]] =[nums[i],nums[i-1]]
-		}
-	}
+function wiggleSort(nums) {
+  for (let i = 1; i < nums.length; i += 1) {
+    if ((i % 2 === 1) === nums[i - 1] > nums[i]) {
+      [nums[i - 1], nums[i]] = [nums[i], nums[i - 1]];
+    }
+  }
 }
