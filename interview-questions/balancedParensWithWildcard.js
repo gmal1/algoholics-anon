e25ca416f0c781e463135863969dafd0c92c0ae9
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-07-10 12:42:05 -0400

    wild parens

--- interview-questions/balancedParensWithWildcard.js
+++ interview-questions/balancedParensWithWildcard.js
@@ -5,7 +5,35 @@ For example, (()* and (*) are balanced. )*( is not balanced.
 */
 
 function wildParens(str, stack = []) {
-  for (let char of str) {
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char === '(') {
+      stack.push('(');
+    } else if (char === ')') {
+      const top = stack.pop();
+      if (top !== '(') return false;
+    } else if (char === '*') {
+      const removed = str.slice(0, i) + str.slice(i + 1);
+      const addedRight = str.slice(0, i) + '(' + str.slice(i + 1);
+      const addedLeft = str.slice(0, i) + ')' + str.slice(i + 1);
+      return wildParens(removed) || wildParens(addedRight) || wildParens(addedLeft);
+    }
   }
   return stack.length === 0 ? true : false;
 }
+
+console.log(wildParens('')); // true
+console.log(wildParens('(')); // false
+console.log(wildParens(')')); // false
+console.log(wildParens('(())()')); // true
+
+console.log(wildParens('*')); // true
+console.log(wildParens('(()*')); // true
+console.log(wildParens('(*)')); // true
+console.log(wildParens(')*(')); // false
+console.log(wildParens('(***))')); // true
+console.log(wildParens('*(*')); // true
+console.log(wildParens('**')); // true
+console.log(wildParens('()*)')); // true
+console.log(wildParens('()*(')); // false
+console.log(wildParens('(*(')); // false
