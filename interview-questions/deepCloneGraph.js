e5d75402f216818b2e9ac44b74a50475edc2898e
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-07-18 20:21:27 -0500

    horrible graph error corrected

--- interview-questions/deepCloneGraph.js
+++ interview-questions/deepCloneGraph.js
@@ -18,7 +18,7 @@ function cloneGraph(graph) {
 
   function traverse(node) {
     if (!node) return
-    if (map.has(node)) {
+    if (!map.has(node)) {
       const newNode = new Node(node.val);
       map.set(node, newNode);
       newNode.neighbors = node.neighbors.map(traverse);
