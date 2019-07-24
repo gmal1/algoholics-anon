c5056c9894f26872745e05153860d8ce837d0692
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-07-24 10:55:19 -0500

    trie with prototype syntax

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,28 +1,59 @@
-class Trie {
-  constructor(words) {
-    this.root = {};
-    words.forEach(word => {
-      this.addWord(word);
-    });
-  }
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+/**
+ * Initialize your data structure here.
+ */
+const Trie = function() {
+  this.root = {};
+};
 
-  addWord(word) {
-    let node = this.root;
-    word.split('').forEach(char => {
-      if (!node[char]) {
-        node[char] = {};
-      }
-      node = nodeChar;
-    });
-    node.word = word;
-  }
-
-  search(word) {
-    let node = this.root;
-    for (let char of word) {
-      if (!node[char]) return false;
-      node = node[char];
+/**
+ * Inserts a word into the trie.
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+  let node = this.root;
+  for (let char of word) {
+    if (!node[char]) {
+      node[char] = {};
     }
-    return node.word === word;
+    node = node[char];
   }
-}
+  node.word = word;
+};
+
+/**
+ * Returns if the word is in the trie.
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+  let node = this.root;
+  for (let char of word) {
+    if (!node[char]) return false;
+    node = node[char];
+  }
+  return node.word === word;
+};
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix.
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+  let node = this.root;
+  for (let char of prefix) {
+    if (!node[char]) return false;
+    node = node[char];
+  }
+  return true;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
