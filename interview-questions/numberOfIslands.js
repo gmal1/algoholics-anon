ee8d7de868ae33b1c08ab4b07b12b3e55d602968
ChristianEdwardPadilla <christianepadilla@gmail.com>
2019-07-26 10:06:53 -0500

    islands

--- interview-questions/numberOfIslands.js
+++ interview-questions/numberOfIslands.js
@@ -20,3 +20,28 @@ Input:
 
 Output: 3
 */
+
+function islands(graph) {
+  let count = 0;
+  for (let y = 0; y < graph.length; y++) {
+    for (let x = 0; x < graph.length; x++) {
+      if (graph[y][x] === 1) {
+        dfs(y, x);
+        count++;
+      }
+    }
+  }
+  return count;
+
+  function dfs(y, x) {
+    if (y > graph.length - 1 || y < 0 || x > graph[0].length - 1 || x < 0) return;
+    if (graph[y][x] !== 1) return;
+    graph[y][x] = count + 2;
+    dfs(y + 1, x);
+    dfs(y - 1, x);
+    dfs(y, x - 1);
+    dfs(y, x + 1);
+  }
+}
+
+console.log(islands([[1, 0, 1], [1, 1, 0], [1, 0, 1]])); // 3
