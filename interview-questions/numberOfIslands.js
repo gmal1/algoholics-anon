75e0cc923eae54359f6a3588677a507724bbc6a2
Christian Padilla <christianepadilla@gmail.com>
2020-01-31 00:56:12 -0800

    fix broken islands, add commentary
    recommended soundtrack: https://www.youtube.com/watch?v=nFxvopurUks

--- interview-questions/numberOfIslands.js
+++ interview-questions/numberOfIslands.js
@@ -21,10 +21,21 @@ Input:
 Output: 3
 */
 
+
+/*
+*input is a 2-dimensional array of 1's and not-1's (see test cases below)
+*/
 function islands(graph) {
+  // the value we're looking for. We'll incrememnt this count each time we find new land (a 1 in the grid)
+  // if we find any value other than 1 (
   let count = 0;
+  
+  // We'll scan from left to right starting in the top left of the graph.
+  // whenever we encounter land (1), we'll start a depth-first search. We'll mutate any fresh land we come across (1 -> 2).
+  // once that island of connected 1's is all mutated the recursive stack will unwind and we'll increment the counter.
+  // once the counter is incremented we'll continue the scan of the graph, ignoring anything other than undiscovered land (1's)
   for (let y = 0; y < graph.length; y++) {
-    for (let x = 0; x < graph.length; x++) {
+    for (let x = 0; x < graph[0].length; x++) {
       if (graph[y][x] === 1) {
         dfs(y, x);
         count++;
@@ -33,15 +44,49 @@ function islands(graph) {
   }
   return count;
 
+  // helper function that, given a coordinate:
+  //      -checks if the coord is even in the graph (exit if not)
+  //      -checks if the coord is fresh land (1) (exit if not)
+  //      -mutates the land and explores in all four directions by making four recursive calls
   function dfs(y, x) {
+    // if this y,x coordinate is outside the edges of the grid, don't explore further
     if (y > graph.length - 1 || y < 0 || x > graph[0].length - 1 || x < 0) return;
+    
+    // if this y,x coordinate does not have a value of 1, it is either water (0), or has been explored already (2)
+    // either way, don't explore further 
+    // (ie, go back up the stack. Maybe this was a recursive call, maybe it is time to step out of this exploration 
+    // and increment the counter for this fully explored island)
     if (graph[y][x] !== 1) return;
-    graph[y][x] = count + 2;
-    dfs(y + 1, x);
-    dfs(y - 1, x);
-    dfs(y, x - 1);
-    dfs(y, x + 1);
+    
+    // mark this coordinate as explored
+    graph[y][x] = 2;
+    
+    // continue exploring recursively in all four directions
+    // (one will be a waste, but we don't know which, so do all four)
+    dfs(y + 1, x); // go down
+    dfs(y - 1, x); // go up
+    dfs(y, x - 1); // go left
+    dfs(y, x + 1); // go right
   }
 }
 
-console.log(islands([[1, 0, 1], [1, 1, 0], [1, 0, 1]])); // 3
+// some tests:
+console.log(islands([[1, 0, 1], 
+                     [1, 1, 0], 
+                     [1, 0, 1]])); // 3
+console.log(islands([[1,0,1,0,0]])); // 2
+console.log(islands([[1],
+                     [0],
+                     [0],
+                     [0],
+                     [1],
+                     [0]])); // 2
+
+console.log(islands([[1, 0, 1, 0, 0], 
+                     [1, 1, 0, 0, 1], 
+                     [1, 0, 1, 0, 1]])); // 4
+
+console.log(islands([[1, 1, 1, 0, 1], 
+                     [1, 0, 1, 0, 0], 
+                     [1, 0, 1, 0, 1],
+                     [1, 0, 1, 1, 1]])); // 2
