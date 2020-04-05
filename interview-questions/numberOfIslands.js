f1ca3482db1fce38a2a879b9f307aa485f2041a0
Christian Padilla <christianepadilla@gmail.com>
2020-04-05 15:58:53 -0700

    Update numberOfIslands.js (#57)

--- interview-questions/numberOfIslands.js
+++ interview-questions/numberOfIslands.js
@@ -27,10 +27,11 @@ Output: 3
 */
 function islands(graph) {
   // the value we're looking for. We'll incrememnt this count each time we find new land (a 1 in the grid)
-  // if we find any value other than 1 (
+  // if we find any value other than 1 it must be water (0)  
+  // or previously explored land (2, see line 63)
   let count = 0;
   
-  // We'll scan from left to right starting in the top left of the graph.
+  // We'll scan from left to right starting at the top-left (0,0) of the graph.
   // whenever we encounter land (1), we'll start a depth-first search. We'll mutate any fresh land we come across (1 -> 2).
   // once that island of connected 1's is all mutated the recursive stack will unwind and we'll increment the counter.
   // once the counter is incremented we'll continue the scan of the graph, ignoring anything other than undiscovered land (1's)
@@ -48,6 +49,7 @@ function islands(graph) {
   //      -checks if the coord is even in the graph (exit if not)
   //      -checks if the coord is fresh land (1) (exit if not)
   //      -mutates the land and explores in all four directions by making four recursive calls
+  //        (could optimize to not bother returning the direction you just came from, but probably not worth it)
   function dfs(y, x) {
     // if this y,x coordinate is outside the edges of the grid, don't explore further
     if (y > graph.length - 1 || y < 0 || x > graph[0].length - 1 || x < 0) return;
@@ -62,7 +64,7 @@ function islands(graph) {
     graph[y][x] = 2;
     
     // continue exploring recursively in all four directions
-    // (one will be a waste, but we don't know which, so do all four)
+    // (one will be a waste since we just came from there, but we don't know which, so do all four)
     dfs(y + 1, x); // go down
     dfs(y - 1, x); // go up
     dfs(y, x - 1); // go left
